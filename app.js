05ba4dd4fcb153c4243af378cad98bbcf9bc0b5b
busybrainx99 <ebohgoodness.t@gmail.com>
2023-05-24 15:34:43 +0100

    Second Commit

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.post("/success.html", function(req,res){
 
 
 
-app.listen(process.env.PORT || 3000 ,function(){
+app.listen(process.env.PORT,function(){
     console.log("server is running on port 3003")
 })
 
